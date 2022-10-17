62adeec011cf5faca97df2f5928820b00de1c48d
Errah Fuschia Gure <errah@annix.com.au>
2022-10-17 21:30:03 +0800

    Router Outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import Home from './routes/home/home.component'
+
+const Navigation =  () => {
+    return (
+        <div>
+            <h1>Navigation</h1>
+            <Outlet />
+            <p>below</p>
+        </div>
+    )
+}
+const Shop =  () => {
+    return (
+        <div>
+            <h1>Shop Page</h1>
+            <p>The shop page</p>
+        </div>
+    )
+}
 
 const App = () => {
   return (
      <Routes>
-       <Route path='/' element={ <Home />} />
+       <Route path='/' element={ <Navigation />}>
+           <Route index element={<Home />} />
+           <Route path='shop' element={<Shop />} />
+       </Route>
      </Routes>
   );
 }
