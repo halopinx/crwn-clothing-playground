8a9f487d4da79acbbf5d1b5d3462a126bb1f01cb
Errah Fuschia Gure <errah@annix.com.au>
2022-10-18 22:05:39 +0800

    add googlesignin redirect method

--- src/routes/signin/signin.component.jsx
+++ src/routes/signin/signin.component.jsx
@@ -1,5 +1,22 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+import {useEffect} from "react";
+import { getRedirectResult } from 'firebase/auth'
+import {
+    auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils'
 const SignIn = () => {
+    useEffect(() => {
+        const loadUsersData = async () => {
+            const response = await getRedirectResult(auth);
+            if (response){
+                const userDocRef = await createUserDocumentFromAuth(response.user)
+            }
+        }
+        loadUsersData();
+    }, [])
+
     const logGoggleUser = async () => {
         const {user} = await signInWithGooglePopup()
         const userDocRef = await createUserDocumentFromAuth(user)
@@ -9,7 +26,8 @@ const SignIn = () => {
         <div>
             This is Sign In Page.
 
-            <button onClick={logGoggleUser}>Sign In</button>
+            <button onClick={logGoggleUser}>Sign In Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In Google Redirect</button>
         </div>
     )
 }
