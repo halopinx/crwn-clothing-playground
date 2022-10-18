d192579921e13fb71c8e36571d2a322cf7c29cc9
Errah Fuschia Gure <errah@annix.com.au>
2022-10-18 16:18:51 +0800

    add firestore config on firebase.utils.js

--- src/routes/signin/signin.component.jsx
+++ src/routes/signin/signin.component.jsx
@@ -1,8 +1,8 @@
-import { signInWithGooglePopup} from '../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
 const SignIn = () => {
     const logGoggleUser = async () => {
-        const response = await signInWithGooglePopup()
-        console.log(response)
+        const {user} = await signInWithGooglePopup()
+        createUserDocumentFromAuth(user)
     }
 
     return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,3 +23,12 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    console.log('userDocRef', userDocRef)
+    const userSnapshot = await getDoc(userDocRef)
+    console.log('userSnapshot', userSnapshot, userSnapshot.exists())
+}
