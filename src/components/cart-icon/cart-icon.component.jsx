de8b627b999d2b8d6bccd23010ba1458ef8079e0
Errah Fuschia Gure <errah@annix.com.au>
2022-11-08 12:27:02 +0800

    add toggle functionality to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,13 @@
+import {useContext} from "react";
 import { ReactComponent as ShoppingCart} from '../../assets/shopping-bag.svg';
+import {CartContext } from "../../contexts/cart.context";
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
+    const {isCartOpen, setIsCartOpen} = useContext(CartContext)
+    const toggleCartHandler = () => setIsCartOpen(!isCartOpen)
     return (
-        <div className='cart-icon-container'>
+        <div className='cart-icon-container' onClick={toggleCartHandler}>
             <ShoppingCart className='shopping-icon' />
             <span className='item-count'>0</span>
         </div>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,14 @@ import {Outlet, Link} from "react-router-dom";
 import './navigation.styles.scss'
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import {UserContext} from "../../contexts/user.context";
+import {CartContext} from "../../contexts/cart.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation =  () => {
     const {currentUser} = useContext(UserContext)
+    const {isCartOpen} = useContext(CartContext)
     console.log(currentUser)
     return (
         <Fragment>
@@ -23,7 +25,7 @@ const Navigation =  () => {
                     }
                     <CartIcon />
                 </div>
-                <CartDropdown />
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
