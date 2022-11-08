427dd8d8bf368c16ae0c306c8899c3204fff6cb2
Errah Fuschia Gure <errah@annix.com.au>
2022-11-08 17:54:21 +0800

    expand cartcontext - addItemToCart and CartItems

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,12 @@
+import {useContext} from "react";
+import {CartContext} from "../../contexts/cart.context";
 import './product-card.styles.scss';
 import Button from '../button/button.component'
 
 const ProductCard = ({product}) => {
+ const {addItemToCart} = useContext(CartContext)
  const { name, price, imageUrl} = product
+ const addProductToCart = () => addItemToCart(product)
  return (
      <div className="product-card-container">
          <img src={imageUrl} alt={`${name}`}/>
@@ -10,7 +14,7 @@ const ProductCard = ({product}) => {
              <span className="name">{name}</span>
              <span className="price">{price}</span>
          </div>
-         <Button buttonType='inverted'>Add To Cart</Button>
+         <Button buttonType='inverted' onClick={addProductToCart}>Add To Cart</Button>
      </div>
  )
 }
