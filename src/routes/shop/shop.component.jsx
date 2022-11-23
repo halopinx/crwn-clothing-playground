ccee47df2003176d7061c4e183514c5b0cfa30cc
Errah Fuschia Gure <errah@annix.com.au>
2022-11-23 17:25:34 +0800

    create category preview component and add to shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,31 +1,20 @@
 import {Fragment, useContext} from "react";
 import './shop.styles.scss'
 import {CategoriesContext} from "../../contexts/categories.context";
-import ProductCard from '../../components/product-card/product-card.component'
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const Shop = () => {
     const {categoriesMap} = useContext(CategoriesContext)
 
     return (
-        <Fragment>
+        <div className='shop-container'>
             {
                 Object.keys(categoriesMap).map((title) => {
-                    return (
-                        <Fragment key={title}>
-                            <h2>{title}</h2>
-                            <div className='products-container'>
-                                {categoriesMap[title].map((product) => {
-                                    return (
-                                        <ProductCard product={product} key={product.id}/>
-                                    )
-                                })
-                                }
-                            </div>
-                        </Fragment>
-                    )
+                    const products = categoriesMap[title]
+                    return <CategoryPreview key={title} title={title} products={products} />
                 })
             }
-        </Fragment>
+        </div>
     )
 }
 
