36b766d421b44cec4ae90dd587935adb07bc2a68
Errah Fuschia Gure <errah@annix.com.au>
2022-11-29 20:33:58 +0800

    add link to category page and fix styling css class

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -3,11 +3,11 @@ import './category-item.styles.scss'
 const CategoryItem = ({category}) => {
     const {title, imageUrl} = category
     return (
-        <div className='category-container'>
+        <div className='category-item-container'>
             <div className='background-image' style={{
                 backgroundImage: `url(${imageUrl})`
             }} />
-            <div className='category-body-container'>
+            <div className='category-item-body-container'>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
